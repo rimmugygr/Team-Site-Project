5010e4a65c5314231bc401d13b596ddc70cf605b
rimmugyr <rimmugygr@gmail.com>
2020-08-03 19:33:06 +0200

    some refactor code

--- TeamSite-Backend/src/main/java/team/back/configuration/SecurityConfiguration.java
+++ TeamSite-Backend/src/main/java/team/back/configuration/SecurityConfiguration.java
@@ -1,69 +1,6 @@
 package team.back.configuration;
 
 
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-//import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-//import org.springframework.security.oauth2.jwt.*;
-
-//
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Value("${auth0.audience}")
-//    private String audience;
-//
-//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-//    private String issuer;
-//
-//    @Bean
-//    JwtDecoder jwtDecoder() {
-//        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
-//                JwtDecoders.fromOidcIssuerLocation(issuer);
-//
-//        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-//        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-//        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-//
-//        jwtDecoder.setJwtValidator(withAudience);
-//
-//        return jwtDecoder;
-//    }
-//
-//
-//
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .mvcMatchers(HttpMethod.GET, "/post**").authenticated()//post view for all
-//                .mvcMatchers(HttpMethod.POST, "/post**").authenticated() //post add for memeber
-//                .mvcMatchers("/runner**").authenticated()
-//                .mvcMatchers("/runner/show**").permitAll()
-//                .mvcMatchers("/race**").authenticated()
-//                .mvcMatchers("/race/start").authenticated()
-//                .mvcMatchers("/race/start/**/**").authenticated()
-//
-//                //.mvcMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
-//                .and()
-//                .oauth2ResourceServer().jwt();
-//    }
-//}
-
-
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.oauth2ResourceServer().jwt();
-//    }
-//}
-//
-//
-
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
@@ -86,19 +23,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private String apiAudience;
     @Value(value = "${auth0.issuer}")
     private String issuer;
-
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//        configuration.setAllowCredentials(true);
-//        configuration.addAllowedHeader("Authorization");
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
 
 
