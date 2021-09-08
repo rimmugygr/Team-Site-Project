d6e821c8b516888f6e8ee23e5eddd4050f5bc96b
rimmugyr <rimmugygr@gmail.com>
2021-09-08 16:46:38 +0200

    In API: Fix: failed to load ApplicationContext bug in PostControllerTest Annotation @WebMvcTest not load spring context

--- TeamSite-Backend/src/test/java/team/back/controllers/PostControllerTest.java
+++ TeamSite-Backend/src/test/java/team/back/controllers/PostControllerTest.java
@@ -3,12 +3,16 @@ package team.back.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,7 +26,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@WebMvcTest(controllers = {PostController.class})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//@WebMvcTest(controllers = {PostController.class})
 class PostControllerTest {
     @Autowired
     MockMvc mvc;
